8d0ef539942f73dd68c3adf4c12bc0a83c8f79fd
gaochongnan <gaochongnan@ed86daca-dac2-11dd-93da-dbb4cac96d7c>
2010-05-17 09:00:49 +0000

    add more jsd orders

--- trunk/easytrader/jsdcmd.py
+++ trunk/easytrader/jsdcmd.py
@@ -1,5 +1,6 @@
-import jsd
+import sys
 import time
+import jsd
 
 session_config = {}
 session_config["tradedbfn"] = "tradeinfo.db"
@@ -10,6 +11,7 @@ session_config["jsdpasswd"] = "123"
 session_config["branchcode"] = ""
 session_config["ordermethod"] = ""
 
+# login
 s = jsd.session(session_config)
 if not s.setup():
     print "Cannot login"
@@ -37,3 +39,55 @@ getcnresp = jsd.GetClientNumResp(s)
 getcnresp.recv()
 print getcnresp.records
 
+doorder = True
+if len(sys.argv) > 1:
+    oid = sys.argv[1]
+    doorder = False
+
+if doorder:
+    oreq = jsd.OrderReq(s)
+    oreq["exchcode"] = "G"
+    oreq["code"] = "IF1006"
+    oreq["buysell"] = "0"
+    oreq["openclose"] = "0"
+    oreq["ifhedge"] = "0"
+    oreq["count"] = "2"
+    oreq["price"] = "2800"
+    oreq["tradenum"] = "00028184"
+    #oreq["tradenum"] = "aaaaaaaaa9201a"
+    oreq.send()
+    oresp = jsd.OrderResp(s)
+    oresp.recv()
+    print oresp.records
+    #print len(oresp.records[0])
+
+qoreq = jsd.QueryOrderReq(s)
+if doorder:
+    qoreq["order_id"] = oresp.records[0][1]
+else:
+    qoreq["order_id"] = oid
+qoreq.send()
+qoresp = jsd.QueryOrderResp(s)
+qoresp.recv()
+print qoresp.records
+print len(qoresp.records[0])
+
+cancelorder = True
+if cancelorder:
+    coreq = jsd.CancelOrderReq(s)
+    coreq["exchcode"] = "G"
+    coreq["code"] = "IF1006"
+    coreq["buysell"] = "0"
+    coreq["openclose"] = "0"
+    coreq["ifhedge"] = "0"
+    coreq["count"] = "2"
+    coreq["price"] = "2800"
+    coreq["order_id"] = oid
+    coreq["cancelcount"] = "2"
+    coreq["syscode"] = "0"
+    coreq.send()
+    coresp = jsd.CancelOrderResp(s)
+    coresp.recv()
+    print coresp.anwser
+    print coresp.records
+    print len(coresp.records[0])
