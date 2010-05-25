e4cb9723a395b57be2f86afd1638b2287f17dfe4
gaochongnan <gaochongnan@ed86daca-dac2-11dd-93da-dbb4cac96d7c>
2010-05-25 09:09:46 +0000

    in a word, implement SIFOrderPushee and related codes, to open and close stock index futures. cancel open/close not done yet.

---
+++ trunk/easytrader/jsdquerydeal.py
@@ -0,0 +1,26 @@
+import sys, datetime, jsd
+
+session_config = {}
+session_config["tradedbfn"] = "tradeinfo.db"
+session_config["jsdserver"] = "172.18.20.71"
+session_config["jsdport"] = 17990
+session_config["jsdaccount"] = "9201"
+session_config["jsdpasswd"] = "123"
+session_config["branchcode"] = ""
+session_config["ordermethod"] = ""
+
+# login
+s = jsd.session(session_config)
+if not s.setup():
+    print "Cannot login"
+    sys.exit(1)
+
+qdreq = jsd.QueryDealReq(s)
+qdreq.send()
+qdresp = jsd.QueryDealResp(s)
+qdresp.recv()
+for r in qdresp.records:
+    print r
+    print
+print len(qdresp.records[0])
+
