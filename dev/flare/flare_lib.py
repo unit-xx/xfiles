dcaa2fb76318f7a5f2d2e6120708355b168f7781
gaochongnan@gmail.com <gaochongnan@gmail.com@ed86daca-dac2-11dd-93da-dbb4cac96d7c>
2013-04-26 07:27:03 +0000

    working on clas engine

--- dev/flare/flare_lib.py
+++ dev/flare/flare_lib.py
@@ -56,9 +56,7 @@ class qserver(MdSpi):
     def OnRtnDepthMarketData(self, depth_market_data):
         # TODO: save quotation here
         self.qlock.acquire()
-
         print depth_market_data.InstrumentID,depth_market_data.BidPrice1,depth_market_data.BidVolume1,depth_market_data.AskPrice1,depth_market_data.AskVolume1,depth_market_data.LastPrice,depth_market_data.Volume,depth_market_data.UpdateTime,depth_market_data.UpdateMillisec
-
         self.qlock.release()
 
     def getquote(self, inst):
@@ -73,23 +71,113 @@ class qserver(MdSpi):
         mdapi.RegisterFront(self.mdcfg.port)
         mdapi.Init()
 
-class engine:
-    def __init__(self, tcap=10):
+class engine(TraderSpi):
+    def __init__(self, tradercfg, tcap=10, orderman):
+        self.broker_id = tradercfg.broker_id
+        self.investor_id = tradercfg.investor_id
+        self.passwd = tradercfg.passwd
+        self.tradercfg = tradercfg
+
+        self.logger = logging.getLogger()
+        self.name = self.__class__.__name__
+
         self.tqueue = Queue.Queue()
+        self.tqlock = Lock()
         self.tcap = tcap
         self.tpool = []
 
+        self.reqid = 1
+        self.reqlock = Lock()
+
+        self.frontid = 0
+        self.sessionid = 0
+        self.oref = 0
+
+        self.orderman = orderman
+
+
+    def isRspSuccess(self, RspInfo):
+        return RspInfo == None or RspInfo.ErrorID == 0
+
+    def OnFrontDisConnected(self, reason):
+        self.logger.info(u'front disconnected, reason:%s' % (reason,))
+
+    def OnFrontConnected(self):
+        self.logger.info(u'front connected')
+        self.user_login(self.broker_id, self.investor_id, self.passwd)
+
+    def user_login(self, broker_id, investor_id, passwd):
+        req = ustruct.ReqUserLogin(BrokerID=broker_id, UserID=investor_id, Password=passwd)
+        r=self.api.ReqUserLogin(req, self.inc_request_id())
+
+    def OnRspUserLogin(self, userlogin, info, rid, is_last):
+        self.logger.info(u'trader login, info:%s, rid:%s, is_last:%s' % (info, rid, is_last))
+        if not self.isRspSuccess(info):
+            self.logger.warning(u'trader login failed')
+            return
+        self.logger.info(u'TD:trader login success')
+        self.front_id = userlogin.FrontID
+        self.session_id = userlogin.SessionID
+        self.order_ref = int(userlogin.MaxOrderRef)
+
     def setup(self):
         # start thread pool
+        # start ctp api
+
+    def inc_request_id(self):
+        ret = 0 
+        with self.reqlock:
+            self.reqid += 1
+            ret = self.reqid
+        return ret
+
+    def inc_order_ref(self):
+        ret = 0 
+        with self.oreflock:
+            self.oref += 1
+            ret = self.oref
+        return ret
+
+    def putorder(self, order):
+        # put order into queue
         pass
 
-    def addtask(self, t):
-        self.tqueue.put(t)
+    def doorder(self, order):
+        req = ustruct.InputOrder(
+                InstrumentID = order.instrument,
+                Direction = order.direction,
+                OrderRef = str(order.order_ref),
+                LimitPrice = order.price,   #有个疑问，double类型如何保证舍入舍出，在服务器端取整?
+                VolumeTotalOriginal = order.volume,
+                OrderPriceType = utype.THOST_FTDC_OPT_LimitPrice,
+                
+                BrokerID = self.cuser.broker_id,
+                InvestorID = self.cuser.investor_id,
+                CombOffsetFlag = utype.THOST_FTDC_OF_Open,         #开仓 5位字符,但是只用到第0位
+                CombHedgeFlag = utype.THOST_FTDC_HF_Speculation,   #投机 5位字符,但是只用到第0位
+
+                VolumeCondition = utype.THOST_FTDC_VC_AV,
+                MinVolume = 1,  #这个作用有点不确定,有的文档设成0了
+                ForceCloseReason = utype.THOST_FTDC_FCC_NotForceClose,
+                IsAutoSuspend = 1,
+                UserForceClose = 0,
+                TimeCondition = utype.THOST_FTDC_TC_GFD,
+            )
+        # TODO: save order in meme for access in callbacks
+        #       add entry in ref2order map
+        #       seems no need to save every orders in db, 
+        #       add virtual account, session mapping at connection
+        #       should be enough (need conform).
+
+        logging.info(u'下单: instrument=%s,方向=%s,数量=%s,价格=%s,OrderRef=%d' % (order.instrument,u'多' if order.direction==utype.THOST_FTDC_D_Buy else u'空',order.volume,order.price,order.order_ref))
+        r = self.trader.ReqOrderInsert(req,self.inc_request_id())
+        return r
 
 class engine_worker(Thread):
-    def __init__(self, tqueue, qserv):
+    def __init__(self, engine, qserv):
         Thread.__init__(self)
-        self.tqueue = tqueue
+        self.tqueue = engine.tqueue
+        self.engine = engine
         self.qserv = qserv
         self.name = self.__class__.__name__
         self.logger = logging.getLogger()
