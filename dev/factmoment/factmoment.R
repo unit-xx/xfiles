9714ef500e72c119ccfa68138a31df22d1619fbb
gaochongnan <gaochongnan@ed86daca-dac2-11dd-93da-dbb4cac96d7c>
2014-03-24 08:21:54 +0000

    check in.

---
+++ dev/factmoment/factmoment.R
@@ -0,0 +1,7 @@
+# calculate factorial moment in rolling windows, draw the result along with price
+
+# params:
+# tick per bin: how many ticks in a bin
+# bin per window: how many bin in a window
+# step: bin differences between consequtive windows.
+# q: q in factorial moment formulae
