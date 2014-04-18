d33d56d2247065d44e01c666d2b4b9aadab543b7
gaochongnan@gmail.com <gaochongnan@gmail.com@ed86daca-dac2-11dd-93da-dbb4cac96d7c>
2014-04-18 07:24:11 +0000

    dual momentum implemented

--- dev/momentum/dualmmt1.R
+++ dev/momentum/dualmmt1.R
@@ -11,13 +11,83 @@ library(zoo)
 # step, rebalance every step days
 # r, rate of cash account
 
+topn = 5
+k1 = 200
+k2 = 1
+rbstep = 21
+rf = 0.03
+
 # read pzoo
 
 qfn = 'citic.level1.csv'
 pzoo = read.zoo(qfn, header=T, sep=',', 
                 colClasses=c('character',rep('numeric',29)))
+pmat = as.matrix(pzoo)
 
+# rebalance points
 
+rbday.prev = 1
+rbday = k1 + 1
+sharevec = rep(0, NCOL(pzoo))
+wealth = rep(0, NROW(pzoo))
+cash = 100
+
+repeat
+{
+  #browser()
+
+  if(rbday > NROW(pmat))
+  {
+    # calc returns from last rebalacne day to the end of day of pmat
+    pseg = pmat[rbday.prev:NROW(pmat),]
+    wseg = pseg %*% sharevec + cash
+    wealth[rbday.prev:NROW(pmat)] = wseg
+
+    break
+  }
+  
+  print(rbday)
+  
+  # calc wealth between rbday and previous rbday
+  
+  pseg = pmat[rbday.prev:rbday,]
+  wseg = pseg %*% sharevec + cash
+  wealth[rbday.prev:rbday] = wseg
+  
+  # sell all existing stocks
+  
+  cash = wealth[rbday]
+  sharevec = rep(0, NCOL(pzoo))
+
+  # re-select topN dual momentum 
+  
+  plookback1 = pmat[(rbday-k1),]
+  plookback2 = pmat[(rbday-k2),]
+  # annualize return in lookback periods
+  retlb = (plookback2 / plookback1 - 1) / (k1 - k2) * 252
+  
+  # TODO: add absolute momentum
+  topnstock = order(retlb, decreasing=T)[1:topn]
+  absmmtstock = which(retlb >= rf)
+  targetstock = intersect(topnstock, absmmtstock)
+  
+  # buy new stocks
+  
+  if(length(targetstock) > 0)
+  {
+    cashvec = rep(0, NCOL(pzoo))
+    cashvec[targetstock] = cash / length(targetstock)
+    sharevec = cashvec / pmat[rbday,]
+    cash = 0
+  }
+
+  # step to next rebalance day
+  
+  rbday.prev = rbday
+  rbday = rbday + rbstep
+}
+
+wzoo = zoo(wealth, index(pzoo))
 
 # back test and generate trading traces
 
