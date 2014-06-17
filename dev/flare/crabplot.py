eadd1c891fa5443a1da9cd1e6132a43fe2583b3a
gaochongnan@gmail.com <gaochongnan@gmail.com@ed86daca-dac2-11dd-93da-dbb4cac96d7c>
2014-06-17 06:19:30 +0000

    add visualfull

--- dev/flare/crabplot.py
+++ dev/flare/crabplot.py
@@ -2,45 +2,55 @@
 
 import sys, os
 
-def doplot(frame):
-    print 'beginframe'
+def doplot(frame, plotf):
+    print >>plotf, 'beginframe'
+
+    eventcnt = 0
     for ii, ff in enumerate(frame):
         if ff['event']=='quote':
             if ff['tag']=='lazy':
-                print 'quote points %d %.2f - black' % (ii, ff['bid1'])
-                print 'quote points %d %.2f - black' % (ii, ff['ask1'])
+                print >>plotf, 'quote points %d %.2f %d black' % (eventcnt, ff['bid1'], ff['bidvol1'])
+                print >>plotf, 'quote points %d %.2f %d black' % (eventcnt, ff['ask1'], ff['askvol1'])
             else:
-                print 'quote points %d %.2f - black' % (ii, ff['bid1'])
-                print 'quote points %d %.2f - black' % (ii, ff['ask1'])
+                print >>plotf, 'quote points %d %.2f %d black' % (eventcnt, ff['bid1'], ff['bidvol1'])
+                print >>plotf, 'quote points %d %.2f %d black' % (eventcnt, ff['ask1'], ff['askvol1'])
 
         elif ff['event']=='setlazy':
-            print 'setlazy points %d %.2f + red' % (ii, ff['setlazybid'])
-            print 'setlazy points %d %.2f + red' % (ii, ff['setlazyask'])
+            print >>plotf, 'setlazy points %d %.2f + red' % (eventcnt, ff['setlazybid'])
+            print >>plotf, 'setlazy points %d %.2f + red' % (eventcnt, ff['setlazyask'])
 
             setlazybid = ff['setlazybid']
             setlazyask = ff['setlazyask']
 
         elif ff['event']=='bidtrade':
-            print 'bidtrade points %d %.2f b red' % (ii, setlazybid)
+            print >>plotf, 'bidtrade points %d %.2f b red' % (eventcnt, setlazybid)
             
         elif ff['event']=='asktrade':
-            print 'asktrade points %d %.2f a red' % (ii, setlazyask)
+            print >>plotf, 'asktrade points %d %.2f a red' % (eventcnt, setlazyask)
 
         elif ff['event']=='orderquick':
             pass
 
         elif ff['event']=='lazytradeinfo':
-            print 'lazytradeinfo points %d %.2f L purple' % (ii, ff['price'])
+            print >>plotf, 'lazytradeinfo points %d %.2f L purple' % (eventcnt, ff['price'])
 
         elif ff['event']=='quicktradeinfo':
-            print 'quicktradeinfo points %d %.2f q purple' % (ii, ff['price'])
-    print 'endframe'
+            print >>plotf, 'quicktradeinfo points %d %.2f q purple' % (eventcnt, ff['price'])
 
-def main():
-    tracefn = sys.argv[1]
-    nhist = int(sys.argv[2])
+        elif ff['event']=='sprdmid':
+            pass
 
-    tf = open(tracefn, 'r')
+        else:
+            pass
+
+        eventcnt += 1
+
+    print >>plotf, 'endframe'
+
+def visualone(tf, plotf, nhist):
+    '''
+    visualize only the last limit order which is traded
+    '''
 
     qhist = []
     frame = []
@@ -70,7 +80,7 @@ def main():
                 framewithtail[i].append(edict)
                 if frametailq[i] == nhist:
                     # TODO: check the frame is correct.
-                    doplot(framewithtail[i])
+                    doplot(framewithtail[i], plotf)
 
                     frametodel.append(i)
 
@@ -121,6 +131,65 @@ def main():
 
         else:
             pass
+
+def visualfull(tf, plotf, nhist):
+    '''
+    visualize every limit order until the one that is traded
+    '''
+
+    qhist = []
+    frame = []
+    fullhist = []
+
+    for line in tf:
+        line = line.strip()
+
+        event, edict = line.split(None, 1)
+        try:
+            edict = eval(edict)
+            edict['event'] = event
+            fullhist.append(edict)
+        except Exception:
+            pass
+
+    for ii, edict in enumerate(fullhist):
+        if edict['event']=='lazytradeinfo':
+            frame = []
+            frame.extend(fullhist[max(0,ii-nhist):min(ii+nhist,len(fullhist))])
+            print >>sys.stderr, max(0,ii-nhist), min(ii+nhist,len(fullhist))
+
+            doplot(frame, plotf)
+
+def main():
+    mode = sys.argv[1]
+    tracefn = sys.argv[2]
+    nhist = int(sys.argv[3])
+
+    plotf = sys.stdout
+    try:
+        plotfn = sys.argv[4]
+        if os.path.exists(plotfn):
+            ans = raw_input('output file exists, overwrite? ')
+            if ans!='yes':
+                print >>sys.stderr, 'I quit.'
+                sys.exit(1)
+
+        plotf = open(plotfn, 'w')
+    except IndexError:
+        pass
+
+    tf = open(tracefn, 'r')
+
+    if mode=='one':
+        visualone(tf, plotf, nhist)
+    elif mode=='full':
+        visualfull(tf, plotf, nhist)
+    else:
+        print 'Unknown mode, mode can be: [one, full]'
+
+    plotf.close()
+    tf.close()
+
 
 if __name__=='__main__':
     main()
--- dev/flare/crabplot.r
+++ dev/flare/crabplot.r
@@ -15,7 +15,10 @@ for(i in 1:NROW(pp))
     endid = i
     toplot = pp[(beginid+1):(endid-1),c(3,4,5,6)]
     
-    plot(toplot[,1], toplot[,2], pch=toplot[,3], col=toplot[,4])
+    plot(toplot[,1], toplot[,2], pch=toplot[,3], col=toplot[,4], type='n')
+    abline(v=seq(0, max(toplot[,1]), 1), col="lightgray", lty="dotted")
+    abline(h=seq(min(toplot[,2]), max(toplot[,2]), 0.2), col="lightgray", lty="dotted")
+    points(toplot[,1], toplot[,2], pch=toplot[,3], col=toplot[,4])
   }
 }
 dev.off()
