86a9f84964d7a95bf2271b73b77d9d344c51f3b5
gaochongnan@gmail.com <gaochongnan@gmail.com@ed86daca-dac2-11dd-93da-dbb4cac96d7c>
2014-09-16 08:03:40 +0000

    add smifixcalc2 and MA of SMI

--- dev/SmartMoneyIndex/SMI.R
+++ dev/SmartMoneyIndex/SMI.R
@@ -1,11 +1,12 @@
 library(zoo)
+library(TTR)
 
 args = commandArgs(T)
 # calculate Smart Money Index. SMI is defined as: 
 
 # read 5min quote data
 
-qfn.base = '000300'
+qfn.base = '000001'
 if(length(args)>0)
 {
   qfn.base = args[1]
@@ -22,23 +23,42 @@ quote$hms = datetime[,2]
 #dailycount = aggregate(quote, by=list(quote$date), FUN=NROW)
 #plot(dailycount$close)
 
-smifixcalc <- function(quote)
+smifixcalc <- function(quote, firstn, lastn)
 {
-  unsmart = quote[6] - quote[1]
-  smart = quote[length(quote)] - quote[length(quote)-12]
+  unsmart = quote[firstn] - quote[1]
+  smart = quote[length(quote)] - quote[length(quote)-lastn]
   #print(unsmart)
   #print(smart)
   return(smart-unsmart)
 }
+
+smifixcalc2 <- function(quote, firstn, lastn)
+{
+  unsmart = quote[firstn] - quote[1]
+  smart = quote[length(quote)] - quote[length(quote)-lastn]
+  #print(unsmart)
+  #print(smart)
+  return(smart+unsmart)
+}
+
+
 # calculate SMI fix for each day
 qavg = (quote$open + quote$high + quote$low + quote$close)/4
 qclose = aggregate(quote$close, by=list(quote$date), FUN=function(quote){return(quote[length(quote)])})[,2]
-smifix = aggregate(qavg, by=list(quote$date), FUN=smifixcalc)[,2]
+smifix = aggregate(qavg, by=list(quote$date), FUN=smifixcalc, 6, 6)[,2]
+smifix2 = aggregate(qavg, by=list(quote$date), FUN=smifixcalc2, 6, 6)[,2]
 
 # add SMI fix to original quote.
-qzoo = zoo(cbind(close=qclose, smifix=smifix, cumfix=cumsum(smifix)), as.Date(unique(quote$date)))
-qzoo$closefix = qzoo$close + qzoo$cumfix
+qzoo = zoo(cbind(close=qclose, smifix=smifix, cumfix=cumsum(smifix), smifix2=smifix2, cumfix2=cumsum(smifix2)), as.Date(unique(quote$date)))
+qzoo$smiroll14 = rollapplyr(qzoo$smifix, 14, sum)
+qzoo$smiroll21 = rollapplyr(qzoo$smifix, 21, sum)
+qzoo$smi2roll14 = rollapplyr(qzoo$smifix2, 14, sum)
+qzoo$smi2roll21 = rollapplyr(qzoo$smifix2, 21, sum)
+
+macd1 = MACD(qzoo$cumfix2, 5,10,5)
+qzoo = cbind(qzoo, macd1)
 
 pdf(paste(qfn.base, 'pdf', sep='.'), width=17.55, height=11.07)
-plot(qzoo[,cbind('close', 'cumfix')], main=qfn.base)
+plot(qzoo[,c('close', 'smiroll21', 'smiroll14')], main=qfn.base)
+plot(qzoo[,c('close', 'smi2roll21', 'smi2roll14')], main=qfn.base)
 dev.off()
